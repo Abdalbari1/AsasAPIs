f431fc1595e0748da807a1bd08b8ee46adda7bff
saud <sa3od34ki@gmail.com>
2026-05-28 22:16:01 +0300

    refactor(auth): migrate login endpoint from API controller to minimal API

---
+++ AsasAPIs/AsasHash/Models/Hash.cs
@@ -0,0 +1,17 @@
+﻿namespace Asas.AsasHash.Models
+{
+    public class Hash
+    {
+        // Stores the encrypted hash string.
+        public string HashedPassword { get; set; } = default!;
+
+        // Stores the plain-text password provided by the user. 
+        // Should only be populated temporarily during the request lifecycle.
+        public string RawPassword { get; set; } = default!;
+
+        // Indicates the success status of the hashing or verification process.
+        public bool IsSucceeded { get; set; } = false;
+
+        public GoodHash GoodHash { get; set; } = default!;
+    }
+}
---
+++ AsasAPIs/EndpointRoute/Login.cs
@@ -0,0 +1,65 @@
+﻿using Asas.AsasHash.Contracts;
+using Asas.AsasHash.Models;
+using Asas.Data;
+using Asas.Models;
+using Azure.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsasAPIs.EndpointRoute
+{
+    public static class Login
+    {
+
+         public record LoginRequest(string Email, string Pass);
+        public static IEndpointRouteBuilder MapLoginEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var loginGroup = builder.MapGroup("/api/Login");
+
+            loginGroup.MapPost("/", async (
+                LoginRequest request,
+                AsasContext dbContext,
+                IAsasHashPassword hashService
+                )// lambda parameters
+              =>
+            {
+                var compAcc = await dbContext.Acc.FirstOrDefaultAsync(a => a.Email == request.Email);
+                if (compAcc == null)
+                {
+                    return Results.BadRequest(new { Message = "البريد الإلكتروني أو كلمة المرور غير صحيحة" });
+                }
+                var loginContext = new Hash
+                {
+                    RawPassword = request.Pass,
+                    HashedPassword = compAcc.Pass,
+                    GoodHash = new GoodHash
+                    {
+                        IV = compAcc.IV,
+                        Iterations = compAcc.Iterations,
+                        Salt = compAcc.Salt
+                    }
+                };
+
+                var verifyResult = hashService.VerifyPassword(loginContext);
+                if (!verifyResult.IsSucceeded)
+                {
+                    return Results.BadRequest(new { Message = "البريد الإلكتروني أو كلمة المرور غير صحيحة" });
+                }
+                return Results.Ok(new { Message = "تم تسجيل الدخول بنجاح" });
+
+            }// lambda
+
+            );//MapPost
+
+               return builder;
+
+        }// IEndpointRouteBuilder
+
+
+
+
+    }
+    
+}
